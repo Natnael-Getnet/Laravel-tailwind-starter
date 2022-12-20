6e8fe783489493a7ab64df09ae71d4318d40f4be
Natnael-Getnet <nantaelgetnetharegewoyn@gmail.com>
2022-12-21 01:41:59 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>
+            @section('title')
+            @show
+        </title>
+        @yield('header_styles')
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+
+    <body>
+        @include('layouts/header')
+
+        @yield('content')
+
+        @include('layouts/footer')
+    </body>
+
+</html>
